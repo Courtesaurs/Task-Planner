09c087a6d401ffb02b47ac031dc3cfe86b149151
Elena <elenafedorets65@gmail.com>
2017-03-26 18:24:24 +0300

    Auth page

---
+++ static/js/ui.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+
+/*-------- Input Animations ----------*/
+
+	$('.input__field').focusout(function(){ 
+		if($.trim($(this).val()) == 0) {
+			$(this).parent().removeClass('input--filled');
+		} 
+	});
+
+	$('.input__field').focusin(function(){
+		$(this).parent().addClass('input--filled');
+	});
+  
+/*------------------------------------ */
+  
+
+});
+
