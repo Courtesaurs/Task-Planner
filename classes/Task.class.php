1f3d6f7534f4b9045c3545ac7c41c083978fd69b
mozhavro <mozhario@gmail.com>
2017-03-28 01:51:44 +0300

    Implemented some Task methods

--- classes/Task.class.php
+++ classes/Task.class.php
@@ -8,19 +8,27 @@
 
 namespace App;
 
+require dirname(__FILE__). "/DataBase.class.php";
+
 
 class Task
 {
     private $id;
-    private $title;
-    private $descritpion;
-    private $status;
-    private $deadline;
+    public $title;
+    public $descritpion;
+    public $status;
+    public $deadline;
 
-    public function __construct($name, $description)
+    public function __construct($title, $description, $status=0, $deadline=false, $id=false)
     {
-        $this->name = $name;
-        $this->description = $description;
+        $this->title = $title;
+        $this->descritpion = $description;
+        $this->status = $status;
+        $this->deadline = $deadline;
+
+        if ($id) {
+            $this->id = $id;
+        }
     }
 
     public function save()
@@ -40,16 +48,54 @@ class Task
 
     public function unassign()
     {
-        
+
     }
 
     public static function get($id)
     {
+        $db = new DataBase();
 
+        try
+        {
+            $sql = "SELECT * FROM task WHERE task.id=".$id;
+            $stmt = $db->pdo->query($sql);
+            $row = $stmt->fetchObject();
+
+            return Task($row->title, $row->description, $row->id);
+
+        }
+        catch (PDOException $e)
+        {
+            echo 'Failed to get the task: ' . $e->getMessage();
+        }
     }
 
-    public static function getObjects($args)
+    public static function getObjects($args=array())
     {
+        $db = new DataBase();
 
+        try
+        {
+            $sql = "SELECT * FROM task";
+            $stmt = $db->pdo->query($sql);
+
+            $tasks = array();
+            foreach($stmt as $row) {
+                $tasks[] = new Task(
+                    $row['title'],
+                    $row['description'],
+                    $row['status'],
+                    $row['deadline'],
+                    $row['id']
+                );
+            }
+
+            return $tasks;
+
+        }
+        catch (PDOException $e)
+        {
+            echo 'Failed to get the task: ' . $e->getMessage();
+        }
     }
 }
