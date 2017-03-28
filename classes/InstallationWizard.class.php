2541758be4d113a56e03d247e7862bf7a1474f08
Саня Вадимович <mozhario@gmail.com>
2017-03-28 19:04:46 +0300

    Installation wizard prototype

---
+++ classes/InstallationWizard.class.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+
+class InstallationWizard
+{
+    public function __construct()
+    {
+    }
+
+    public function checkForCredentials()
+    {
+    }
+
+    public function saveCredentials()
+    {
+    }
+
+    public function createAdminAccount()
+    {
+    }
+}
