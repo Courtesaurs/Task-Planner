f0f0ce8b244f9dc36b22b007744b2e296847f369
Nick Lysenko <mazanicker@gmail.com>
2017-03-30 00:24:58 +0300

    Authorization completed

--- auth.php
+++ auth.php
@@ -7,16 +7,37 @@ require_once dirname(__FILE__) . '/classes/User.class.php';
 $login = trim(htmlspecialchars(stripslashes($_POST['login'])));
 $password = trim(htmlspecialchars(stripslashes($_POST['password'])));
 
-$user = new \App\User(
-	$login,
-	$login,
-	1,
-	$password
-);
+if (isset($_POST['sign-up'])) {
 
-if( $user->save() ) {
-	$_SESSION['login'] = $login;
+	$user = new \App\User(
+		$login,
+		$login,
+		1,
+		$password
+	);
+
+	if( $user->save() ) {
+		$_SESSION['login'] = $login;
+	}
+
+	header("Location: /");
+	die();
+} elseif (isset($_POST['sign-in'])) {
+
+	$user = new \App\User(
+		$login,
+		$login,
+		1,
+		$password
+	);
+
+	if( $user->login() ) {
+		$_SESSION['login'] = $login;
+	}
+
+	header("Location: /");
+	die();
+	
+} else {
+	exit('What are you even doing here?');
 }
-
-header("Location: /");
-die();
